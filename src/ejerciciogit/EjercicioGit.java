450905b459b133b644281f8fb2285bd7a669f5d8
dam <dam@203P03>
2024-02-15 08:55:00 +0100

    Líneas en el método main

---
+++ src/ejerciciogit/EjercicioGit.java
@@ -0,0 +1,13 @@
+
+package ejerciciogit;
+
+
+public class EjercicioGit {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Visualizar 1");
+        System.out.println("Visualizar 2");
+    }
+    
+}
